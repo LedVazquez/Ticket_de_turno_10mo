5d2e0728be766124b5abebf39413f920e0578964
jarmarj <jarmarj_first@hotmail.com>
2023-03-31 14:44:58 -0600

    validacion de solicitud.php

--- public/js/val_ticket.js
+++ public/js/val_ticket.js
@@ -1,57 +1,67 @@
-const formulario = document.getElementById('formulario');
-const inputs = formulario.getElementsByTagName('input');
+function validate() {    
+    curp = document.getElementById("curp").value;
+    nombre = document.getElementById("nombres").value;
+    paterno = document.getElementById("apellido_paterno").value;
+    materno = document.getElementById("apellido_materno").value;
+    telefono = document.getElementById("telefono").value;
+    celular = document.getElementById("celular").value;
+    email = document.getElementById("email").value;
 
-formulario.addEventListener('submit', (event) => {
-    event.preventDefault();
-    validarCampos();
-});
-
-function validarCampos() {
-    let errores = [];
-
-    for (let i = 0; i < inputs.length; i++) {
-        if (!inputs[i].value) {
-            errores.push(`El campo ${inputs[i].name} es obligatorio.`);
-        }
+    
+    if (!validateNombres(nombre)) {
+      alert("El nombre debe tener entre 3 y 30 caracteres.");
+      return;
     }
-
-    if (errores.length > 0) {
-        mostrarErrores(errores);
-    } else {
-        enviarFormulario();
+    if (!validateNombres(paterno)) {
+      alert("El apellido paterno debe tener entre 3 y 30 caracteres.");
+      return;
     }
-}
-
-function mostrarErrores(errores) {
-    const mensaje = errores.join('\n');
-    alert(mensaje);
-}
-
-function enviarFormulario() {
-    formulario.submit();
-}
-
-// Función para validar la fecha de nacimiento
-function validarFecha() {
-    const fechaInput = document.getElementById("fecha");
-    const fecha = new Date(fechaInput.value);
-    const ahora = new Date();
-    if (fecha > ahora) {
-      alert("La fecha de nacimiento no puede ser en el futuro.");
-      return false;
+    if (!validateNombres(materno)) {
+      alert("El apellido materno debe tener entre 3 y 30 caracteres.");
+      return;
     }
-    return true;
+    if (!validateCurp(curp)) {
+        alert("CURP no cumple los requisitos");
+        return;
+      }
+    if (!validiateTel(telefono)) {
+      alert("El telefono debe tener 10 digitos");
+      return;
+    }
+    if (!validiateTel(celular)) {
+      alert("El celular debe tener 10 digitos");
+      return;
+    }
+    if (!validateEmail(email)) {
+      alert("El correo no cumple los requisitos");
+      return;
+    }
+  
+    document.getElementById('check').style.display = 'none';
+    document.getElementById('submit').style.display = 'block'
+  }
+  function validateCurp(curp) {
+    let re = new RegExp(
+      "[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}" +
+        "(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])" +
+        "[HM]{1}" +
+        "(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)" +
+        "[B-DF-HJ-NP-TV-Z]{3}" +
+        "[0-9A-Z]{1}[0-9]{1}$"
+    );
+    return re.test(curp);
   }
   
-  // Agrega la validación de la fecha al evento "submit" del formulario
-  const solicitud = document.getElementById("formulario");
-  formulario.addEventListener("submit", function (event) {
-    event.preventDefault(); // previene que se envíe el formulario
+  function validateNombres(nom) {
+    let re = new RegExp("[a-zA-Z ]{3,30}?$");
+    return re.test(nom);
+  }
   
-    // Valida los campos del formulario
-    if (validarNombres() && validarApellidoPaterno() && validarApellidoMaterno() && validarCurp() && validarTelefono() && validarCelular() && validarEmail() && validarFecha()) {
-      // Envía el formulario
-      formulario.submit();
-    }
-  });
-  
+  function validiateTel(num) {
+    let re = new RegExp("\\d{10}");
+    return re.test(num);
+  }
+  function validateEmail(email) {
+    let re = new RegExp('^.+@[^\.].*\.[a-z]{2,}$');
+    return re.test(email);
+  }
--- views/solicitud.php
+++ views/solicitud.php
@@ -1,110 +1,102 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8" />
-    <title>Solicitud Ticket</title>
-    <link rel="stylesheet" href="../public/css/style.css" />
-    <link rel="stylesheet" href="../public/css/bootstrap/bootstrap.css">
-  </head>
-  <script src="js/val_ticket.js"></script>
-  <body>
-    <section>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="landing.php">Home</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="login.php">Log In</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="solicitud.php">Crear</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="consulta.php">Buscar</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="editar.php">Editar</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>          
-  </section>
-    <div class="contenedor">
-      <form class="formulario" id="formulario">
-        <h2>Solicitud Ticket</h2>
-        <div class="campo">
-          <label for="nombres">Nombres:</label>
-          <input type="text" id="nombres" name="nombres" required />
-        </div>
-        <div class="campo">
-          <label for="apellido_paterno">Apellido paterno:</label>
-          <input
-            type="text"
-            id="apellido_paterno"
-            name="apellido_paterno"
-            required
-          />
-        </div>
-        <div class="campo">
-          <label for="apellido_materno">Apellido materno:</label>
-          <input
-            type="text"
-            id="apellido_materno"
-            name="apellido_materno"
-            required
-          />
-        </div>
-        <div class="campo">
-          <label for="curp">CURP:</label>
-          <input type="text" id="curp" name="curp" required />
-        </div>
-        <div class="campo">
-          <label for="telefono">Teléfono:</label>
-          <input type="text" id="telefono" name="telefono" required />
-        </div>
-        <div class="campo">
-          <label for="celular">Celular:</label>
-          <input type="text" id="celular" name="celular" required />
-        </div>
-        <div class="campo">
-          <label for="email">Correo electrónico:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div class="campo">
-          <label for="fecha">Fecha de nacimiento:</label>
-          <input type="date" id="fecha" name="fecha" required />
-        </div>        
-        <div>
-            <label for="nivel">Nivel:</label>
-            <select id="nivel" name="nivel">
-              <option value="preparatoria">Preparatoria</option>
-              <option value="universidad">Universidad</option>
-              <option value="maestria">Maestria</option>
-            </select>
-          </div>
-        <div>
-          <label for="municipio">Municipio:</label>
-          <select id="municipio" name="municipio">
-          <?php
-              include ("../controllers/municipioController.php");
-              $munic = new municipios;
-              $municipios=$munic->get_municipios();
 
-              foreach ($municipios as $municipio){
-                  echo "<option>a</option>";
-              }
-          ?>
-          </select>
+<head>
+  <meta charset="UTF-8" />
+  <title>Solicitud Ticket</title>
+  <link rel="stylesheet" href="../public/css/style.css" />
+  <link rel="stylesheet" href="../public/css/bootstrap/bootstrap.css">
+</head>
+
+<body>
+  <section>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="landing.php">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Log In</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="solicitud.php">Crear</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="consulta.php">Buscar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="editar.php">Editar</a>
+            </li>
+          </ul>
         </div>
-        <div class="campo">
-          <button type="submit">Obtener número de ticket</button>
-        </div>
-      </form>
-    </div>
-  </body>
+      </div>
+    </nav>
+  </section>
+  <div class="contenedor">
+    <form method="post" class="formulario" id="formulario">
+      <h2>Solicitud Ticket</h2>
+      <div class="campo">
+        <label for="nombres">Nombres:</label>
+        <input type="text" id="nombres" name="nombres" required />
+      </div>
+      <div class="campo">
+        <label for="apellido_paterno">Apellido paterno:</label>
+        <input type="text" id="apellido_paterno" name="apellido_paterno" required />
+      </div>
+      <div class="campo">
+        <label for="apellido_materno">Apellido materno:</label>
+        <input type="text" id="apellido_materno" name="apellido_materno" required />
+      </div>
+      <div class="campo">
+        <label for="curp">CURP:</label>
+        <input type="text" id="curp" name="curp" required />
+      </div>
+      <div class="campo">
+        <label for="telefono">Teléfono:</label>
+        <input type="text" id="telefono" name="telefono" required />
+      </div>
+      <div class="campo">
+        <label for="celular">Celular:</label>
+        <input type="text" id="celular" name="celular" required />
+      </div>
+      <div class="campo">
+        <label for="email">Correo electrónico:</label>
+        <input type="email" id="email" name="email" required />
+      </div>
+      <div class="campo">
+        <label for="fecha">Fecha de nacimiento:</label>
+        <input type="date" id="fecha" name="fecha" required />
+      </div>
+      <div>
+        <label for="nivel">Nivel:</label>
+        <select id="nivel" name="nivel">
+          <option value="preparatoria">Preparatoria</option>
+          <option value="universidad">Universidad</option>
+          <option value="maestria">Maestria</option>
+        </select>
+      </div>
+      <div>
+        <label for="municipio">Municipio:</label>
+        <select id="municipio" name="municipio">
+          <option value="saltillo">Saltillo</option>
+          <option value="arteaga">Arteaga</option>
+          <option value="ramosarizpe">Ramos Arizpe</option>
+          <option value="torreon">Torreon</option>
+          <option value="acuna">Acuña</option>
+        </select>
+      </div>
+      <div class="campo">
+        <button class="btn btn-primary" id="check" style="display:block" type="button" onclick="validate()">Check</button>
+        <button id="submit" style="display:none" type="submit">Obtener número de ticket</button>
+      </div>
+    </form>
+
+  </div>
+  <script src="../public/js/val_ticket.js"></script>
+
+</body>
+
 </html>
