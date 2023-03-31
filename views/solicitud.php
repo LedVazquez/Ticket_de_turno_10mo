8077c6bb2d01c26ab076f9e0d4596d73fae6b982
Adrián Vázquez <adrianlvh00@gmail.com>
2023-03-31 15:12:02 -0600

    [CHANGE] quitado el duplicado

--- views/solicitud.php
+++ views/solicitud.php
@@ -90,58 +90,7 @@
           <label for="municipio">Municipio:</label>
           <select id="municipio" name="municipio">
           
-          <option value="">Saltillo</option>
-          </select>
-        </div>
-      </div>
-    </nav>
-  </section>
-  <div class="contenedor">
-    <form method="post" class="formulario" id="formulario">
-      <h2>Solicitud Ticket</h2>
-      <div class="campo">
-        <label for="nombres">Nombres:</label>
-        <input type="text" id="nombres" name="nombres" required />
-      </div>
-      <div class="campo">
-        <label for="apellido_paterno">Apellido paterno:</label>
-        <input type="text" id="apellido_paterno" name="apellido_paterno" required />
-      </div>
-      <div class="campo">
-        <label for="apellido_materno">Apellido materno:</label>
-        <input type="text" id="apellido_materno" name="apellido_materno" required />
-      </div>
-      <div class="campo">
-        <label for="curp">CURP:</label>
-        <input type="text" id="curp" name="curp" required />
-      </div>
-      <div class="campo">
-        <label for="telefono">Teléfono:</label>
-        <input type="text" id="telefono" name="telefono" required />
-      </div>
-      <div class="campo">
-        <label for="celular">Celular:</label>
-        <input type="text" id="celular" name="celular" required />
-      </div>
-      <div class="campo">
-        <label for="email">Correo electrónico:</label>
-        <input type="email" id="email" name="email" required />
-      </div>
-      <div class="campo">
-        <label for="fecha">Fecha de nacimiento:</label>
-        <input type="date" id="fecha" name="fecha" required />
-      </div>
-      <div>
-        <label for="nivel">Nivel:</label>
-        <select id="nivel" name="nivel">
-          <option value="preparatoria">Preparatoria</option>
-          <option value="universidad">Universidad</option>
-          <option value="maestria">Maestria</option>
-        </select>
-      </div>
-      <div>
-        <label for="municipio">Municipio:</label>
-        <select id="municipio" name="municipio">
+          
           <option value="saltillo">Saltillo</option>
           <option value="arteaga">Arteaga</option>
           <option value="ramosarizpe">Ramos Arizpe</option>
